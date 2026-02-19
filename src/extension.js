97f0c93cea623497233defc25becbde2951316de
Pawel Zayakin <pz@magisters.net>
2026-02-19 22:43:00 +0000

    gnome 49

--- src/extension.js
+++ src/extension.js
@@ -1,104 +1,123 @@
-const Gio = imports.gi.Gio;
-const Shell = imports.gi.Shell;
-const AppDisplay = imports.ui.appDisplay;
+import Gio from "gi://Gio";
+import Shell from "gi://Shell";
+// noinspection JSFileReferences
+import * as AppDisplay from "resource:///org/gnome/shell/ui/appDisplay.js";
 
-// Get Gnome-shell version
-const Config = imports.misc.config;
-const [major, minor] = Config.PACKAGE_VERSION.split('.').map(s => Number(s));
+let originalGetInitialResultSet = null;
 
-var originalGetInitialResultSet = null;
+const cyrillicToLatin = new Map([
+    ['а', 'a'],
+    ['б', 'b'],
+    ['в', 'w'],
+    ['г', 'g'],
+    ['д', 'd'],
+    ['е', 'e'],
+    ['ё', '#'],
+    ['ж', 'v'],
+    ['з', 'z'],
+    ['и', 'i'],
+    ['й', 'j'],
+    ['к', 'k'],
+    ['л', 'l'],
+    ['м', 'm'],
+    ['н', 'n'],
+    ['о', 'o'],
+    ['п', 'p'],
+    ['р', 'r'],
+    ['с', 's'],
+    ['т', 't'],
+    ['у', 'u'],
+    ['ы', 'y'],
+    ['х', 'h'],
+    ['ф', 'f'],
+    ['ц', 'c'],
+    ['ч', '='],
+    ['ш', '['],
+    ['щ', ']'],
+    ['ь', 'x'],
+    ['ъ', '^'],
+    ['э', '\\'],
+    ['ю', '`'],
+    ['я', 'q]'
+]);
 
+const latinToCyrillic = new Map();
 
-const TrancodeRusToEngDict = {
-    'а': 'a',
-    'б': 'b',
-    'в': 'w',
-    'г': 'g',
-    'д': 'd',
-    'е': 'e',
-    'ё': '#',
-    'ж': 'v',
-    'з': 'z',
-    'и': 'i',
-    'й': 'j',
-    'к': 'k',
-    'л': 'l',
-    'м': 'm',
-    'н': 'n',
-    'о': 'o',
-    'п': 'p',
-    'р': 'r',
-    'с': 's',
-    'т': 't',
-    'у': 'u',
-    'ы': 'y',
-    'х': 'h',
-    'ф': 'f',
-    'ц': 'c',
-    'ч': '=',
-    'ш': '[',
-    'щ': ']',
-    'ь': 'x',
-    'ъ': '^',
-    'э': '\\',
-    'ю': '`',
-    'я': 'q'
+function generateInvertedDict(sourceMap, destMap) {
+    sourceMap.forEach((value, key) => {
+        destMap.set(value, key);
+    });
 }
 
-const TrancodeEngToRusDict = {};
-
-function generateIvertedDict(sourceDict, destDict) {
-    for(let sourceindex in sourceDict)  {
-        destDict[sourceDict[sourceindex]] = sourceindex;
-    }
-}
-
-function transcode(source, dict) {
+function transcode(source, transcodeCharMap) {
     source = source.toLowerCase();
-    let result = '';
-    for (let i = 0; i < source.length; i++) {
-        let char = source.charAt(i);
-        let foundChar = dict[char];
-        if (!foundChar) {
-            foundChar = char;
-        }
-        result = result + foundChar;
-    }
-    return result;
+    return [...source]
+        .map((char) => transcodeCharMap.get(char) || char)
+        .join("");
 }
 
-function getResultSet(terms, callback, cancellable) {
-    let query = terms.join(' ');
-    let groups = Gio.DesktopAppInfo.search(query);
-    groups = groups.concat(Gio.DesktopAppInfo.search(transcode(query, TrancodeRusToEngDict)));
-    groups = groups.concat(Gio.DesktopAppInfo.search(transcode(query, TrancodeEngToRusDict)));
+function getResultSet(terms) {
+    let query = terms.join(" ");
+    let groups = [];
+    const systemCommands = [
+        "restart",
+        "poweroff",
+        "shutdown",
+        "reboot",
+        "logout",
+    ];
+
+    if (systemCommands.includes(query.toLowerCase())) {
+        return originalGetInitialResultSet.call(
+            AppDisplay.AppSearchProvider,
+            terms
+        );
+    }
+    try {
+        groups = Gio.DesktopAppInfo.search(query);
+        groups = groups.concat(
+            Gio.DesktopAppInfo.search(transcode(query, cyrillicToLatin))
+        );
+        groups = groups.concat(
+            Gio.DesktopAppInfo.search(transcode(query, latinToCyrillic))
+        );
+    } catch (error) {
+        console.error("An error occurred while searching:", error);
+    }
+
     let usage = Shell.AppUsage.get_default();
     let results = [];
-    groups.forEach(function(group) {
-        group = group.filter(function(appID) {
+    groups.forEach(function (group) {
+        group = group.filter(function (appID) {
             let app = Gio.DesktopAppInfo.new(appID);
             return app && app.should_show();
         });
-        results = results.concat(group.sort(function(a, b) {
-            return usage.compare('', a, b);
-        }));
+        results = results.concat(
+            group.sort(function (a, b) {
+                return usage.compare(a, b);
+            })
+        );
     });
-    if (major >= 43)
-        return results;
-    else
-        callback(results);
+    return results;
 }
 
-function init() {
-}
+export default class TranscodeAppSearchExtension {
+    enable() {
+        if (originalGetInitialResultSet === null) {
+            generateInvertedDict(cyrillicToLatin, latinToCyrillic);
+            originalGetInitialResultSet =
+                AppDisplay.AppSearchProvider.prototype.getInitialResultSet;
+            AppDisplay.AppSearchProvider.prototype.getInitialResultSet =
+                getResultSet;
+        }
+    }
 
-function enable() {
-    generateIvertedDict(TrancodeRusToEngDict, TrancodeEngToRusDict);
-    originalGetInitialResultSet = AppDisplay.AppSearchProvider.prototype.getInitialResultSet;
-    AppDisplay.AppSearchProvider.prototype.getInitialResultSet = getResultSet;
-}
-
-function disable() {
-    AppDisplay.AppSearchProvider.prototype.getInitialResultSet = originalGetInitialResultSet;
-    originalGetInitialResultSet = null;
+    disable() {
+        if (originalGetInitialResultSet !== null) {
+            AppDisplay.AppSearchProvider.prototype.getInitialResultSet =
+                originalGetInitialResultSet;
+            originalGetInitialResultSet = null;
+        }
+        latinToCyrillic.clear();
+    }
 }
